9824bd743374a9baef0fa8be901360e8abc3786e
Nick Sutterer <apotonick@gmail.com>
2014-08-27 09:48:30 +1000

    allow model.image(file){} to encapsulate processing an upload and saving metadata in the model.

--- lib/paperdragon/model.rb
+++ lib/paperdragon/model.rb
@@ -13,8 +13,12 @@ module Paperdragon
       # Creates Avatar#image that returns a Paperdragon::File instance.
       def attachment_accessor_for(name, attachment_class)
         mod = Module.new do # TODO: abstract that into Uber, we use it everywhere.
-          define_method name do
-            attachment_class.new(self.image_meta_data, {:model => self})
+          define_method name do |file=nil, &block|
+            attachment = attachment_class.new(self.image_meta_data, {:model => self})
+
+            return attachment unless file
+
+            self.image_meta_data = attachment.task(file, &block)
           end
         end
       end
--- test/model_test.rb
+++ test/model_test.rb
@@ -22,14 +22,24 @@ class PaperdragonModelTest < MiniTest::Spec
 
 
   # minimum setup
-  class Image
+  class Image < OpenStruct
     include Paperdragon::Model
     processable :image
-
-    def image_meta_data
-      {:thumb => {:uid => "Avatar-thumb"}}
-    end
   end
 
-  it { Image.new.image[:thumb].url.must_equal "/paperdragon/Avatar-thumb" }
+  it { Image.new(:image_meta_data => {:thumb => {:uid => "Avatar-thumb"}}).image[:thumb].url.must_equal "/paperdragon/Avatar-thumb" }
+
+
+  # process with #image{}
+  let (:logo) { Pathname("test/fixtures/apotomo.png") }
+
+  it do
+    model = Image.new
+    model.image(logo) do |v|
+      v.process!(:original)
+      v.process!(:thumb) { |j| j.thumb!("16x16") }
+    end
+
+    model.image_meta_data.must_equal({:original=>{:width=>216, :height=>63, :uid=>"original-apotomo.png"}, :thumb=>{:width=>16, :height=>5, :uid=>"thumb-apotomo.png"}})
+  end
 end
