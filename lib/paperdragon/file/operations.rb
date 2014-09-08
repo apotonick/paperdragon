eda28f016c04a48cf509acfb7784949be916e2ed
Nick Sutterer <apotonick@gmail.com>
2014-09-09 08:23:00 +1000

    rename fingerprint to new_uid

--- lib/paperdragon/file/operations.rb
+++ lib/paperdragon/file/operations.rb
@@ -25,13 +25,13 @@ module Paperdragon
 
 
     module Reprocess
-      def reprocess!(fingerprint, original, metadata={})
+      def reprocess!(new_uid, original, metadata={})
         job = Dragonfly.app.new_job(original.data) # inheritance here somehow?
 
         yield job if block_given?
 
         old_uid = uid
-        uid!(fingerprint) # new UID is computed and set.
+        uid!(new_uid) # new UID is already computed and set.
 
         puts "........................STORE  (reprocess): #{uid}"
         job.store(path: uid, headers: {'x-amz-acl' => 'public-read', "Content-Type" => "image/jpeg"}) # store with thumb url.
